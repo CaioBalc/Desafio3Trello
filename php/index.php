61bda4c8cb22f0befe58c0bc655e291b4afc3059
Igor-Flesch-Correa <igorfleschcorrea@gmail.com>
2024-02-08 09:38:51 -0300

    index

--- php/index.php
+++ php/index.php
@@ -4,3 +4,10 @@ require "Atribuicao.php";
 require "Projeto.php";
 require "Tarefa.php";
 require "Usuario.php";
+
+$dadosUsuario = [
+    'nome_usuario' => 'NomeDoUsuario',
+    'email' => 'email@exemplo.com'
+];
+
+?>
