dd887581d9c4c757e5f79fd5e6ee70926ac779c5
CaioBalc <balczarekcaio@gmail.com>
2024-02-08 10:38:34 -0300

    Commit

--- php/index.php
+++ php/index.php
@@ -5,10 +5,7 @@ require "Projeto.php";
 require "Tarefa.php";
 require "Usuario.php";
 
-$dadosUsuario1 = [
-    'nome_usuario' => 'NomeDoUsuario',
-    'email' => 'email@exemplo.com'
-];
+
 $usuario1 = new Usuario();
 $usuario1->criaUsuario($dadosUsuario1);
 
