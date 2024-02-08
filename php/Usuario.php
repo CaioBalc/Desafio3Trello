bd40708e9b25add8bd9059f83c4d81634fdf963e
Igor-Flesch-Correa <igorfleschcorrea@gmail.com>
2024-02-08 09:06:40 -0300

    Update Usuario.php

--- php/Usuario.php
+++ php/Usuario.php
@@ -1,5 +1,34 @@
 <?php
+require_once 'Conexao.php'; 
 
 class Usuario{
+    private $conexaoBanco;
+
+    public function __construct($sessionToken) {
+        $this->conexaoBanco = Conexao::conectar(); // Utiliza a conexão única fornecida pela classe Conexao
+    }
+
+    private function salvaUsuario($questionData) {//função usada dentro da de cima para pegar os dados do json e salvar no formato certo na tabela
+        $sql = "INSERT INTO perguntas (tipo, categoria, dificuldade, pergunta, respostaCorreta, respostasIncorretas) VALUES (?, ?, ?, ?, ?, ?)";
+        try 
+        {
+            $stmt = $this->conexaoBanco->prepare($sql);
+            $incorrectAnswers = implode(', ', $questionData['incorrect_answers']);
+            $stmt->execute([
+                $questionData['type'],
+                $questionData['category'],
+                $questionData['difficulty'],
+                $questionData['question'],
+                $questionData['correct_answer'],
+                $incorrectAnswers
+            ]);
+          
+            echo "Usuario salvo com sucesso.\n";
+        } 
+        catch (PDOException $e) 
+        {
+            exit('Erro ao salvar pergunta no banco de dados: ' . $e->getMessage());
+        }
+    }
 
 }
