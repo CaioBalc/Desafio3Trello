768e6395da1d5f95a8cd8243c15f1aa86909ed48
CaioBalc <balczarekcaio@gmail.com>
2024-02-08 10:51:49 -0300

    Update index.php

--- php/index.php
+++ php/index.php
@@ -10,5 +10,5 @@ $usuario1 = new Usuario();
 $usuario1->criaUsuario('Nome Usuario','email@teste.com');
 
 $projeto = new Projeto();
-$projeto->criaProjeto('Nome projeto', 'Descrição projeto', '01/01/0001', '12/12/0012');
+$projeto->criaProjeto('Nome projeto', 'Descrição projeto', '2024-02-05', '2024-02-09');
 ?>
