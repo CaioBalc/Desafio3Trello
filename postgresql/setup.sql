bea893f0f375a951608fbe668783ed237630f1d2
Igor-Flesch-Correa <igorfleschcorrea@gmail.com>
2024-02-08 11:25:52 -0300

    Update setup.sql

--- postgresql/setup.sql
+++ postgresql/setup.sql
@@ -1,33 +1,32 @@
 
-CREATE TABLE projetos(
-    id_projeto SERIAL PRIMARY KEY NOT NULL,
+CREATE TABLE usuarios (
+    id_usuario SERIAL PRIMARY KEY,
+    nome_usuario VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL 
+);
+
+CREATE TABLE projetos (
+    id_projeto SERIAL PRIMARY KEY,
     nome_projeto VARCHAR(255) NOT NULL,
     descricao_projeto TEXT,
     data_inicio DATE,
     data_fim DATE
 );
 
-
-CREATE TABLE tarefas(
-    id_tarefa SERIAL PRIMARY KEY NOT NULL,
+CREATE TABLE tarefas (
+    id_tarefa SERIAL PRIMARY KEY,
     descricao_tarefa TEXT NOT NULL,
     id_projeto INT,
     data_inicio DATE,
     data_fim DATE,
-    FOREIGN KEY (id_projeto) REFERENCES projetos(id_projeto)
+    FOREIGN KEY (id_projeto) REFERENCES projetos (id_projeto) ON DELETE CASCADE -- Ação em cascata ao deletar um projeto
 );
 
-CREATE TABLE atribuicoes(
-    id_atribuicoes SERIAL PRIMARY KEY NOT NULL,
+CREATE TABLE atribuicoes (
+    id_atribuicao SERIAL PRIMARY KEY,
     id_usuario INT,
     id_tarefa INT,
     data_atribuicao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario),
-    FOREIGN KEY (id_tarefa) REFERENCES tarefas(id_tarefa)
-);
-
-CREATE TABLE usuarios(
-    id_usuario SERIAL PRIMARY KEY NOT NULL,
-    nome_usuario VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL
+    FOREIGN KEY (id_usuario) REFERENCES usuarios (id_usuario) ON DELETE CASCADE,
+    FOREIGN KEY (id_tarefa) REFERENCES tarefas (id_tarefa) ON DELETE CASCADE
 );
