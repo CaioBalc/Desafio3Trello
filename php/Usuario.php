41948a62320bcf37d4a357094961a7000e964b41
Igor-Flesch-Correa <igorfleschcorrea@gmail.com>
2024-02-08 09:23:06 -0300

    pronto

--- php/Usuario.php
+++ php/Usuario.php
@@ -8,19 +8,20 @@ class Usuario{
         $this->conexaoBanco = Conexao::conectar(); // Utiliza a conexão única fornecida pela classe Conexao
     }
 
-    private function salvaUsuario($questionData) {//função usada dentro da de cima para pegar os dados do json e salvar no formato certo na tabela
-        $sql = "INSERT INTO usuarios (nome, email) VALUES (?, ?)";
+    private function criaUsuario()
+    {
+        
+    }
+    private function salvaUsuario($usuarioData) {//função usada dentro da de cima para pegar os dados do json e salvar no formato certo na tabela
+        $sql = "INSERT INTO usuarios (nome_usuario, email) VALUES (?, ?)";
         try 
         {
             $stmt = $this->conexaoBanco->prepare($sql);
-            $incorrectAnswers = implode(', ', $questionData['incorrect_answers']);
             $stmt->execute([
-                $questionData['type'],
-                $questionData['category'],
-                $questionData['difficulty'],
-                $questionData['question'],
-                $questionData['correct_answer'],
-                $incorrectAnswers
+
+                $usuarioData['nome_usuario'],
+                $usuarioData['email'],
+   
             ]);
           
             echo "Usuario salvo com sucesso.\n";
