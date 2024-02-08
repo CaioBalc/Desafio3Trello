ed2ecf496c1c12b8350b5e8e1619fb5a8fe9d96b
Igor-Flesch-Correa <igorfleschcorrea@gmail.com>
2024-02-08 11:18:22 -0300

    Update index.php

--- php/index.php
+++ php/index.php
@@ -1,7 +1,5 @@
 <?php
-//sudo chmod -R 755 /home/imply/Área\ de\ Trabalho/desafios/desafios02/desafio2/dados     se por acaso acontecer de novo
-//sudo chown -R imply /home/imply/Área\ de\ Trabalho/desafios/desafios02/desafio2/dados
-//remover dados
+
 
 require "Atribuicao.php";
 require "Projeto.php";
@@ -15,6 +13,6 @@ $usuario1->criaUsuario('Nome Usuario','email@teste.com');
 $projeto = new Projeto();
 $projeto->criaProjeto('Nome projeto', 'Descrição projeto', '2024-02-05', '2024-02-09');
 
-echo "FUNCIONOU!";
+
 #echo $projeto;
 ?>
