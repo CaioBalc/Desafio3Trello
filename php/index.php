7ebc619136412e01bbde2fe40095e5fdb45047e9
Igor-Flesch-Correa <igorfleschcorrea@gmail.com>
2024-02-08 09:42:41 -0300

    update

--- php/index.php
+++ php/index.php
@@ -5,9 +5,11 @@ require "Projeto.php";
 require "Tarefa.php";
 require "Usuario.php";
 
-$dadosUsuario = [
+$dadosUsuario1 = [
     'nome_usuario' => 'NomeDoUsuario',
     'email' => 'email@exemplo.com'
 ];
+$usuario1 = new Usuario();
+$usuario1->criaUsuario($dadosUsuario1);
 
 ?>
