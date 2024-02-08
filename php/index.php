41998afccde5f8632180269546a76047b7b757be
CaioBalc <balczarekcaio@gmail.com>
2024-02-08 10:59:07 -0300

    Commit

--- php/index.php
+++ php/index.php
@@ -11,4 +11,7 @@ $usuario1->criaUsuario('Nome Usuario','email@teste.com');
 
 $projeto = new Projeto();
 $projeto->criaProjeto('Nome projeto', 'Descrição projeto', '2024-02-05', '2024-02-09');
+
+echo "FUNCIONOU!";
+#echo $projeto;
 ?>
