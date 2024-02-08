e8657b483128fc6acf42c8a1cb335169fcd76813
CaioBalc <balczarekcaio@gmail.com>
2024-02-08 20:20:17 -0300

    Commit

--- php/Tarefa.php
+++ php/Tarefa.php
@@ -13,18 +13,18 @@ class Tarefa {
         $this->conexaoBanco = Conexao::conectar(); // Utiliza a conexão única fornecida pela classe Conexao
     }
 
-    public function criaTarefa($tarefaData) {
+    public function criaTarefa($descricao_tarefa, $data_inicio, $data_fim, $id_projeto) {
         try {
             // Validação básica dos dados da tarefa
-            if (empty($tarefaData->descricao_tarefa)) {
+            if (empty($descricao_tarefa)) {
                 throw new Exception("A descrição da tarefa é obrigatória.");
             }
 
             // Atribui os dados validados aos atributos da classe
-            $this->descricao_tarefa = $tarefaData->descricao_tarefa;
-            $this->data_inicio = $tarefaData->data_inicio;
-            $this->data_fim = $tarefaData->data_fim;
-            $this->id_projeto = $tarefaData->id_projeto;
+            $this->descricao_tarefa = $descricao_tarefa;
+            $this->data_inicio = $data_inicio;
+            $this->data_fim = $data_fim;
+            $this->id_projeto = $id_projeto;
 
             // Salva a tarefa no banco de dados
             $this->salvaTarefa();
@@ -42,9 +42,9 @@ class Tarefa {
             $stmt = $this->conexaoBanco->prepare($sql);
             $stmt->execute([
                 $this->descricao_tarefa,
-                $this->id_projeto,
                 $this->data_inicio,
-                $this->data_fim
+                $this->data_fim,
+                $this->id_projeto
             ]);
     
             // Captura o ID da tarefa recém-criada
